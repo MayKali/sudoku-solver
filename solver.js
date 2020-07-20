62dcaaf50e7feb1da06c00e8cdd0e958c5031815
Mohamad Abbas <40679821+MayKali@users.noreply.github.com>
2020-07-20 13:31:52 -0700

    Update solver.js

--- solver.js
+++ solver.js
@@ -18,3 +18,87 @@
 //	likely, the algorithm can go back to a point where its slots are still valid and explore
 //	other possiblities. 
 
+
+//	null value is used to represent an empty space on the board 
+
+const b = null 
+
+//	2D js array to represent a sudoku board
+
+const board = [
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b]
+]
+
+//	Test board 
+const t_board = [
+	[1, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, 8, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, 3, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, 9]
+]
+
+// Impossible to solve, contradiction
+const Impossible = [
+	[1, 2, 3, 4, 5, 6, 7, 8, b],
+	[b, b, b, b, b, b, b, b, 2],
+	[b, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, 4],
+	[b, b, b, b, b, b, b, b, 5],
+	[b, b, b, b, b, b, b, b, 6],
+	[b, b, b, b, b, b, b, b, 7],
+	[b, b, b, b, b, b, b, b, 8],
+	[b, b, b, b, b, b, b, b, 9]
+]
+
+
+const Impossible = [
+	[1, 2, 3, 4, 5, 6, 7, 8, b],
+	[b, b, b, b, b, b, b, b, 1],
+	[b, b, b, b, b, b, b, b, 2],
+	[b, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, 4],
+	[b, b, b, b, b, b, b, b, 5],
+	[b, b, b, b, b, b, b, b, 6],
+	[b, b, b, b, b, b, b, b, 7],
+	[b, b, b, b, b, b, b, b, 8]
+]
+
+
+function solve(board) {
+
+	if (solved(board)) {
+		return board
+	} else {
+		const possiblities = nextBoards(boards)
+		const validBoards = keepOnlyValid(possibilities)
+		return searchForSolution(validBoards)
+	}
+}
+
+function searchForSolution(boards) {
+	if (boards.length < 1) {
+		return false
+	} else {
+		var first = boards.shift()
+		const tryPath = solve(first)
+
+		if(tryPath != false) {
+			return tryPath
+		} else {
+			searchForSolution(boards)
+		}
+	}
+}
