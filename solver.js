819c45632368a33b2135855cdb77fd9e9b3360b4
Mohamad Abbas <40679821+MayKali@users.noreply.github.com>
2020-07-22 17:18:26 -0700

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,81 +1,80 @@
-//	Explanation of Algorithm used:
+/*
+	Explanation of Algorithm used:
 
-//	Objective: Goes through each empty slot and fills it with a number from 1-9.
-//	(Generative Recursion): We are essentially creating a tree with all the possible 
-//							empty values that a slot can hold.
+	Objective: Goes through each empty slot and fills it with a number from 1-9.
+	(Generative Recursion): We are essentially creating a tree with all the possible 
+							empty values that a slot can hold.
 	
 
-//	This will obviously be really memory intensive on the machine, which is why
-//	certain searching algorithms and methods will be necessary. 
+	This will obviously be really memory intensive on the machine, which is why
+	certain searching algorithms and methods will be necessary. 
 
 
-//	We will first need a validator to verify that the number in place is valid with
-//	with the rules of the game i.e. The number inputted cannot be the same as a number
-//	that already exists on the same row, column or square. This is necessary to greatly 
-//	decrease the number of possibilities available, thus making it less memory intensive. 
-
-//	Back-tracking is used to admit our mistakes :), once a dead-end is reached, which is very
-//	likely, the algorithm can go back to a point where its slots are still valid and explore
-//	other possiblities. 
-
+	We will first need a validator to verify that the number in place is valid with
+	with the rules of the game i.e. The number inputted cannot be the same as a number
+	that already exists on the same row, column or square. This is necessary to greatly 
+	decrease the number of possibilities available, thus making it less memory intensive. 
+	Back-tracking is used to admit our mistakes :), once a dead-end is reached, which is very
+	likely, the algorithm can go back to a point where its slots are still valid and explore
+	other possiblities. 
+*/
 
 //	null value is used to represent an empty space on the board 
 
-// // ____DATA DEFINITIONS____ // //
+const b = null 
 
-// Board is List[List[Int]] or false
-// interp. as a sudoku board, or false if the given board is invalid
-// EXAMPLE BOARDS
+//	2D js array to represent a sudoku board
 
-const b = null
+const board = [
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b]
+]
 
-var bd1 = [ [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+//	Possible Test Boards
+const brd1 = [
+	[1, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, 8, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, b],
+	[b, 3, b, b, b, b, b, b, b],
+	[b, b, b, b, b, b, b, b, 9]
+]
 
-
-var bd2 = [ [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b],
-            [b, b, b, b, b, b, b, b, b]]
-
-
-var bd3 = [ [b, b, b, b, b, 8, 9, 1, b],
-            [b, b, 1, b, b, b, b, b, 3],
-            [9, b, b, b, 2, 7, b, b, 5],
-            [3, b, 2, 5, 6, b, b, b, b],
-            [5, b, b, b, b, b, b, b, 8],
-            [b, b, b, b, 8, 3, 5, b, 4],
-            [8, b, b, 7, 4, b, b, b, 2],
-            [6, b, b, b, b, b, 1, b, b],
-            [b, 5, 7, 3, b, b, b, b, b]]
-
-
-var bd4 = [ [1, 2, 3, 4, 5, 6, 7, 8, b],
-            [b, b, b, b, b, b, b, b, 2],
-            [b, b, b, b, b, b, b, b, 3],
-            [b, b, b, b, b, b, b, b, 4],
-            [b, b, b, b, b, b, b, b, 5],
-            [b, b, b, b, b, b, b, b, 6],
-            [b, b, b, b, b, b, b, b, 7],
-            [b, b, b, b, b, b, b, b, 8],
-            [b, b, b, b, b, b, b, b, 9]]
+// Impossible to solve, contradiction
+const brd2 = [
+	[1, 2, 3, 4, 5, 6, 7, 8, b],
+	[b, b, b, b, b, b, b, b, 2],
+	[b, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, 4],
+	[b, b, b, b, b, b, b, b, 5],
+	[b, b, b, b, b, b, b, b, 6],
+	[b, b, b, b, b, b, b, b, 7],
+	[b, b, b, b, b, b, b, b, 8],
+	[b, b, b, b, b, b, b, b, 9]
+]
 
 
-
-
-// // ____FUNCTION DEFINITIONS____ // //
+const brd3 = [
+	[1, 2, 3, 4, 5, 6, 7, 8, b],
+	[b, b, b, b, b, b, b, b, 1],
+	[b, b, b, b, b, b, b, b, 2],
+	[b, b, b, b, b, b, b, b, 3],
+	[b, b, b, b, b, b, b, b, 4],
+	[b, b, b, b, b, b, b, b, 5],
+	[b, b, b, b, b, b, b, b, 6],
+	[b, b, b, b, b, b, b, b, 7],
+	[b, b, b, b, b, b, b, b, 8]
+]
 
 function initiate() {
     // null -> null
@@ -106,212 +105,150 @@ function initiate() {
     }
 }
 
+
 function solve(board) {
-    // THIS FUNCTION WORKS.
-    // Board -> Board
-    // solves the given sudoku board
-    // ASSUME the given sudoku board is valid
-    if (solved(board)) {
-        return board
-    }
-    else {
-        const possibilities = nextBoards(board)
-        const validBoards = keepOnlyValid(possibilities)
-        return searchForSolution(validBoards)
-    }
-}
 
-// ______TESTS______ //
-// console.log(solve(bd4))
-// ______TESTS______ //
+// Checks whether or not the board is solved.
 
-
-function searchForSolution(boards){
-    // List[Board] -> Board or false
-    // finds a valid solution to the sudoku problem
-    if (boards.length < 1){
-        return false
-    }
-    else {
-        // backtracking search for solution
-        var first = boards.shift()
-        const tryPath = solve(first)
-        if (tryPath != false){
-            return tryPath
-        }
-        else{
-            return searchForSolution(boards)
-        }
-    }
+	if (solved(board)) {
+		return board
+	} else {
+		const possiblities = nextBoards(boards)
+//	Gets rid of the invalid boards and implements the back-tracking search
+		const validBoards = keepOnlyValid(possibilities)
+		return searchForSolution(validBoards)
+	}
 }
 
 
-function solved(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
-    // checks to see if the given puzzle is solved
-    for (var i = 0; i < 9; i++){
-        for (var j = 0; j < 9; j++){
-            if (board[i][j] == null){
-                return false
-            }
-        }
-    }
-    return true
+function searchForSolution(boards) {
+
+//	Checks whether there are any valid boards, if there aren't then a solution does not exist
+	if (boards.length < 1) {
+		return false
+	} else {
+		//	Back-tracking search for solution
+		var first = boards.shift()
+		const tryPath = solve(first)
+
+		if(tryPath != false) {
+			return tryPath
+		} else {
+			return searchForSolution(boards)
+		}
+	}
 }
 
-// ______TESTS______ //
-// console.log(solved(bd3))
-// ______TESTS______ //
-
-
-function nextBoards(board){ 
-    // THIS FUNCTION WORKS.
-    // Board -> List[Board]
-    // finds the first emply square and generates 9 different boards filling in that square with numbers 1...9
-    var res = []
-    const firstEmpty = findEmptySquare(board)
-    if (firstEmpty != undefined){
-        const y = firstEmpty[0]
-        const x = firstEmpty[1]
-        for (var i = 1; i <= 9; i++){
-            var newBoard = [...board]
-            var row = [...newBoard[y]]
-            row[x] = i
-            newBoard[y] = row
-            res.push(newBoard)
-        }
-    }
-    return res
+//	Checks whether a board is solved or not
+function solved(board) {
+//	A board is solved when all the slots in a given board return a non-null value. 
+	for (var i = 0; i < 9; i++) {
+		for (var j = 0; j < 9 ; j++) {
+			if (board[i][j] == null) {
+				return null
+			}
+		}
+	}
+	return true
 }
 
-function findEmptySquare(board){
-    // THIS FUNCTION WORKS.
-    // Board -> [Int, Int] 
-    // (get the i j coordinates for the first empty square)
-    for (var i = 0; i < 9; i++){
-        for (var j = 0; j < 9; j++){
-            if (board[i][j] == null) {
-                return [i, j]
-            }
-        }
-    }
+
+// Generate the tree of possiblities from a sudoku board
+function nextBoards(board){
+	// Store all the possible sudoku boards
+	var res = []
+	// Returns a coordinate on the 2D plane 
+	const firstEmpty = findEmptySquare(board) // <-- (y,x)
+	// Generates a list of possibilities 
+	if (firstEmpty != undefined){
+		const y = firstEmpty[0]
+		const x = firstEmpty[1]
+
+		for (var i = 1; i <= 9; i++) {
+	// Arrays are treated like objects, when an array is assigned to a variable, what is stored
+	// is a reference to the array, if I change stuff in the variable, the origin is also changed
+			var newBoard = [...board]
+			var row = [...newBoard[y]]
+			row[x] = i
+			newBoard[y] = row
+			res.push(newBoard)
+		}
+	}
+	return res
 }
 
-// ______TESTS______ //
-// console.log(nextBoards(bd3))
-// console.log(findEmptySquare(bd3))
-// ______TESTS______ //
 
-function keepOnlyValid(boards){
-    // THIS FUNCTION WORKS.
-    // List[Board] -> List[Board]
-    // filters out all of the invalid boards from the list
-    var res = []
-    for (var i = 0; i < boards.length; i++){
-        if (validBoard(boards[i])){
-            res.push(boards[i])
-        }
-    }
-    return res
+function findEmptySquare(board) {
+	// board -> [Int, Int]
+	for (var i = 0; i < 9 ; i++) {
+		for (var j = 0; j < 9 ; j++) {
+			if (board[i][j] == null) {
+				return [i, j]
+			}
+		}
+	}
 }
 
-// ______TESTS______ //
-// console.log(keepOnlyValid([bd1, bd2, bd3]))
-// ______TESTS______ //
-
-
-function validBoard(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
-    // checks to see if given board is valid
-    return rowsGood(board) && columnsGood(board) && boxesGood(board)
+function keepOnlyValid(boards) {
+//	If the valid method returns true, then the board will be kept, else it will be discarded
+	return boards.filter((b => validBoards(b)))
 }
 
-function rowsGood(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
-    // makes sure there are no repeating numbers for each row
-    for (var i = 0; i < 9; i++){
-        var cur = []
-        for (var j = 0; j < 9; j++){
-            if (cur.includes(board[i][j])){
-                return false
-            }
-            else if (board[i][j] != null){
-                cur.push(board[i][j])
-            }
-        }
-    }
-    return true
+// Checks that the rows, columns and boxes do not contain any duplicate numbers 
+function validBoard(board) {
+	return rowGood(board) && columnGood(board) && boxesGood(board)
 }
 
-function columnsGood(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
-    // makes sure there are no repeating numbers for each column
-    for (var i = 0; i < 9; i++){
-        var cur = []
-        for (var j = 0; j < 9; j++){
-            if (cur.includes(board[j][i])){
-                return false
-            }
-            else if (board[j][i] != null){
-                cur.push(board[j][i])
-            }
-        }
-    }
-    return true
+function rowsGood(board) {
+	for (var i = 0; i < 9; i++) {
+		var cur = []
+		for (var j = 0; j < 9; j++) {
+			if (cur.includes(board[i][j])) {
+				return false
+			} else if (board[i][j] != null) {
+				cur.push(board[i][j])
+			}
+		}
+	}
+	return true
+}
+
+function columnGood(board) {
+		for (var i = 0; i < 9; i++) {
+		var cur = []
+		for (var j = 0; j < 9; j++) {
+			if (cur.includes(board[j][ii])) {
+				return false
+			} else if (board[j][i] != null) {
+				cur.push(board[j][i])
+			}
+		}
+	}
+	return true
 }
 
 
 function boxesGood(board){
-    // transform this everywhere to update res
-    const boxCoordinates = [[0, 0], [0, 1], [0, 2],
-                            [1, 0], [1, 1], [1, 2],
-                            [2, 0], [2, 1], [2, 2]]
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
-    // makes sure there are no repeating numbers for each box
-    for (var y = 0; y < 9; y += 3){
-        for (var x = 0; x < 9; x += 3){
-            // each traversal should examine each box
-            var cur = []
-            for (var i = 0; i < 9; i++){
-                var coordinates = [...boxCoordinates[i]]
-                coordinates[0] += y
-                coordinates[1] += x
-                if (cur.includes(board[coordinates[0]][coordinates[1]])){
-                    return false
-                }
-                else if (board[coordinates[0]][coordinates[1]] != null){
-                    cur.push(board[coordinates[0]][coordinates[1]])
-                }
-            }
-        }
-    }
-    return true
+	const boxCoordinates = [
+	[0,0], [0,1], [0,2],
+	[1,0], [1,1], [1,2],
+	[2,0], [2,1], [2,2]
+	]
+
+	for (var y = 0; y < 9; y += 3) {
+		for (var x = 0; x < 9; x += 3) {
+			var cur = []
+			for (var i = 0; i < 9; i++) {
+				var coordinates = [...boxCoordinates[i]]
+				coordinates[0] += y
+				coordinates[1] += x
+				if (cur.includes(board[coordinates[0]][coordinates[1]])) {
+					return false
+				} else if (board[coordinates[0]][coordinates[1]] != null) {
+					cur.push((board[coordinates[0]][coordinates[1]]))
+				}
+	return true
 }
-
-// ______TESTS______ //
-// console.log("Rows:")
-// console.log(rowsGood(bd1))
-// console.log(rowsGood(bd2))
-// console.log(rowsGood(bd3))
-// console.log("Columns:")
-// console.log(columnsGood(bd1))
-// console.log(columnsGood(bd2))
-// console.log(columnsGood(bd3))
-// console.log("Boxes:")
-// console.log(boxesGood(bd1))
-// console.log(boxesGood(bd2))
-// console.log(boxesGood(bd3))
-// console.log("Valid?")
-// console.log(validBoard(bd1))
-// console.log(validBoard(bd2))
-// console.log(validBoard(bd3))
-// ______TESTS______ //
-
 
 function updateBoard(board) {
     // THIS FUNCTION WORKS.
